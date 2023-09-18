ff993755766a54ea970ec7c9a7438aa83ae71353
Debraj Dhar <debrajdhar100@gmail.com>
2023-09-18 23:00:22 +0530

    started chapter 3

---
+++ 3. Laying-the-foundation/coding/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "its an h1 tag"),
+    React.createElement("h2", {}, "its an h2 tag"),
+  ])
+);
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
