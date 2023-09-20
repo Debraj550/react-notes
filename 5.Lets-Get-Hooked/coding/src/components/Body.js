3e25b8cee0b91790cc1e3ed4f003c4ea2300e1ba
Debraj Dhar <debrajdhar100@gmail.com>
2023-09-20 18:02:51 +0530

    added top restaurant feature

--- 5.Lets-Get-Hooked/coding/src/components/Body.js
+++ 5.Lets-Get-Hooked/coding/src/components/Body.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { restaurantList } from "../data/data";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
+  const [allRestaurants, setAllRestaurants] = useState([...restaurantList]);
+
+  const handleToprated = () => {
+    let tempData = [...allRestaurants];
+    tempData = tempData.filter((res) => res.data.avgRating > 4);
+    setAllRestaurants(tempData);
+  };
+
+  const handleAll = () => {
+    setAllRestaurants([...restaurantList]);
+  };
+
   return (
     <div className="body">
-      <div className="search">Search bar</div>
+      <div className="fliter">
+        <button className="all-btn" onClick={handleAll}>
+          All Restaurants
+        </button>
+        <button className="filter-btn" onClick={handleToprated}>
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {restaurantList.map((restaurant) => {
+        {allRestaurants.map((restaurant) => {
           return (
             <RestaurantCard key={restaurant.data.id} {...restaurant.data} />
           );
