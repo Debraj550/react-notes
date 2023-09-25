80a6c9c76b1804e383199a51a3a4a317afa27c41
Debraj Dhar <debrajdhar100@gmail.com>
2023-09-25 17:32:19 +0530

    added styling

--- 11.Data-is-the-new-oil/coding/src/components/RestaurantCard.js
+++ 11.Data-is-the-new-oil/coding/src/components/RestaurantCard.js
@@ -11,21 +11,30 @@ const RestaurantCard = ({
   avgRating,
 }) => {
   return (
-    <div className="px-2 py-2 w-[300px]">
-      <img className="rounded-lg mb-2" src={CDN_URL + cloudinaryImageId} />
-      <h3 className="font-serif font-bold text-xl mb-2">{name}</h3>
-      <h4 className="italic mb-2 text-md font-bold flex flex-wrap overflow-x-clip">
-        {cuisines?.toString()}
-      </h4>
-      <h4 className="font-bold my-1">{area}</h4>
-      <span>
-        <h4 className="my-1">
+    <div className="px-2 py-2 w-[300px] gap-1 items-center">
+      <div>
+        <img className="rounded-lg mb-2" src={CDN_URL + cloudinaryImageId} />
+      </div>
+      <div>
+        <h3 className="font-serif font-bold text-lg">{name}</h3>
+      </div>
+
+      <div>
+        <h4 className="text-gray-500 text-md  flex flex-wrap overflow-x-clip">
+          {cuisines?.toString()}
+        </h4>
+      </div>
+      <div>
+        <h4 className="text-gray-500 ">{area}</h4>
+      </div>
+      <div className="flex gap-4">
+        <h5 className="text-md">
           <i className="fa-solid fa-star"></i>
           {avgRating}
-        </h4>
-        <h4 className="my-1">{lastMileTravelString}</h4>
-        <h4 className="my-1">{costForTwoString}</h4>
-      </span>
+        </h5>
+        <h5 className="">{lastMileTravelString}</h5>
+        <h5 className="">{costForTwoString}</h5>
+      </div>
     </div>
   );
 };
